44b754610f7de6fb364302534f44ffa7d10dc64d
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2022-04-11 22:34:27 -0400

    Created QWERTY keyboard using the Map Function
    Added basic styling so that the keboard is composed of 3 rows as opposed to one 1 row.

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,3 +1,4 @@
+import { keyboard } from '@testing-library/user-event/dist/keyboard'
 import React, {Component} from 'react'
 import '../css/Keyboard.css'
 
@@ -7,9 +8,30 @@ class Keyboard extends Component {
     }
 
     render() {
+
+        let letters = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+        const Ploc = letters.indexOf('P')
+        const Lloc = letters.indexOf('L')
+
+        let keyboardButtons = letters.map((letter, i) =>  
+        //    (i == Ploc || Lloc) ?  <p className="letters">{letter}</p> <br /> : <p className="letters">{letter}</p>
+            {
+                if(i === Ploc || i === Lloc) {
+                   return <React.Fragment><p className="letters">{letter}</p><br /></React.Fragment>
+                   
+                } else {
+                   return <p className="letters">{letter}</p>
+                }
+            }
+        )
+
+        console.log(keyboardButtons)
+
         return(
             <div className="keyboard-inputs">
-                <div>Q</div> <div>W</div> <div>E</div> <div>R</div> <div>T</div> <div>Y</div>
+                {/* <div>Q</div> <div>W</div> <div>E</div> <div>R</div> <div>T</div> <div>Y</div> */}
+                {/* <p>This a test and only a</p> <p>is</p> <p>a</p> <p>test</p> */}
+                {keyboardButtons}
             </div>
         )
     }
